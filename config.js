51fa84a661a8d51b15d0362f6e755671b42694dc
petsuuw677 <michealjoana1xiq@gmail.com>
2026-09-24 17:39:32 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// MoniUsed - Supabase connection
+const SUPABASE_URL = "https://xulzsqeyshyqabpmvszy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Lim8ECXV-KrbTCrDKnXEww_EKgoq6z-";
+
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
